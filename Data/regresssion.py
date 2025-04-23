b7d5f03a4de1faf012dd9b41636d168022b31f68
monatagelsir7 <monatagelsir777@gmail.com>
2025-04-22 21:43:40 -0400

    reg

--- Data/regresssion.py
+++ Data/regresssion.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import statsmodels.formula.api as smf
+import statsmodels.api as sm
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from statsmodels.graphics.gofplots import qqplot
+from statsmodels.stats.diagnostic import het_breuschpagan
+import numpy as np
 
 df = pd.read_parquet(
     r"C:\Users\DELL\UDS Project\IDS701_Team_Project\Data\cleaned_data.parquet"
@@ -39,14 +44,32 @@ daily_agg = (
     .reset_index()
 )
 
-# --- Step 3: Create 'high_load' Flag ---
+# --- Step 3: Create relevant variables ---
 # Define high_load as days with task_count > median daily tasks across all couriers
 median_daily_tasks = daily_agg["task_count"].median()
 daily_agg["high_load"] = (daily_agg["task_count"] > median_daily_tasks).astype(int)
+# categorize the times
+daily_agg["hour_bin"] = pd.cut(
+    daily_agg["delivery_hour_mode"],
+    bins=[0, 6, 12, 18, 24],
+    labels=["Night", "Morning", "Afternoon", "Evening"],
+)
+# transform delivery duration to log and replace zeros with a small value (0.001) + remove outliers
+daily_agg["log_duration"] = np.log(
+    daily_agg["avg_delivery_duration_min"].replace(0, 0.001)
+)
+lower = daily_agg["log_duration"].quantile(0.01)
+upper = daily_agg["log_duration"].quantile(0.99)
+daily_agg["log_duration"] = daily_agg["log_duration"].clip(lower, upper)
+# standarize numeric variables
+# scaler = StandardScaler()
+# daily_agg[["task_count", "avg_distance_km"]] = scaler.fit_transform(
+#     daily_agg[["task_count", "avg_distance_km"]]
+# )
 
 # --- Step 4: Run Regression ---
 model = smf.ols(
-    formula="np.log(avg_delivery_duration_min) ~ task_count * high_load + avg_distance_km + C(delivery_hour_mode)",
+    formula="log_duration ~ task_count + np.power(task_count, 2) + avg_distance_km + task_count * avg_distance_km + C(hour_bin)",
     data=daily_agg,
 ).fit(cov_type="HC3")
 
@@ -57,8 +80,8 @@ print(model.summary())
 # Check for multicollinearity
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 
-X = daily_agg[["task_count", "high_load", "avg_distance_km"]]  # Exclude categorical
-X["interaction"] = X["task_count"] * X["high_load"]  # Add interaction term
+X = daily_agg[["task_count", "high_load", "avg_distance_km"]]
+X["interaction"] = X["task_count"] * X["avg_distance_km"]  # Add interaction term
 X = sm.add_constant(X)
 print(
     pd.DataFrame(
@@ -75,3 +98,11 @@ plt.xlabel("Fitted Values")
 plt.ylabel("Residuals")
 plt.title("Residual Plot")
 plt.show()
+
+# qq plot
+qqplot(model.resid, line="s")
+plt.show()
+
+# Breusch-Pagan Test for heteroscedasticity
+bp_test = het_breuschpagan(model.resid, model.model.exog)
+print("P-value:", bp_test[1])  # <0.05 confirms heteroscedasticity
